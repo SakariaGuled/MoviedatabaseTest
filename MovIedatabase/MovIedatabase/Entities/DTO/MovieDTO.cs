811dd9b3d58025577da4e83d9af9e002243fb51a
SakariaGuled <122364595+SakariaGuled@users.noreply.github.com>
2024-02-20 12:04:27 +0100

    Update MovieDTO.cs

--- MovIedatabase/MovIedatabase/Entities/DTO/MovieDTO.cs
+++ MovIedatabase/MovIedatabase/Entities/DTO/MovieDTO.cs
@@ -1,6 +1,6 @@
 ﻿namespace MovIedatabase.Entities.DTO
 {
     
-        public record MovieDTO(int Id, string Title, string director, long Gross);
+        public record MovieDTO(int Id, string Title, string director, long Gross, string mainActor);
    
 }
