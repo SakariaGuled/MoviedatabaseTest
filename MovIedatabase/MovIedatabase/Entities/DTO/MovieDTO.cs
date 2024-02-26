70061616d845fe98a7d364b56db1d1018b67b59a
Robbelure <robinliverpool@live.no>
2024-02-26 11:43:03 +0100

    Oppdaters Models
    Oppdatert MovieEntity og MovieDTO

--- MovIedatabase/MovIedatabase/Entities/DTO/MovieDTO.cs
+++ MovIedatabase/MovIedatabase/Entities/DTO/MovieDTO.cs
@@ -1,6 +1,17 @@
 ﻿namespace MovIedatabase.Entities.DTO
 {
-    
-        public record MovieDTO(int Id, string Title, string director, long Gross, string mainActor, DateTime ReleaseDate);
-   
+    public record MovieDTO(
+        int Id,
+        string Title,
+        DateTime? ReleaseDate,
+        string Director,
+        string Genre,
+        string Language,
+        long? Gross,
+        string MainActor,
+        string Description,
+        string Country,
+        int? Duration
+    );
+
 }
--- MovIedatabase/MovIedatabase/Entities/MovieEntity.cs
+++ MovIedatabase/MovIedatabase/Entities/MovieEntity.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 public class MovieEntity
 {
     [Key]
@@ -7,15 +9,20 @@ public class MovieEntity
     public string Title { get; set; } = string.Empty;
 
     [Required]
-    public DateTime ReleaseDate { get; set; }
+    public DateTime? ReleaseDate { get; set; }
 
     [Required]
     public string Director { get; set; } = string.Empty;
 
     [Required]
-    public string Genre { get; set; } = string.Empty; 
+    public string Genre { get; set; } = string.Empty;
+
+    [Required]
+    public string Language { get; set; } = string.Empty; 
 
     public string MainActor { get; set; } = string.Empty;
-
-    public long Gross { get; set; } = string.Empty;
+    public long? Gross { get; set; }
+    public string Description { get; set; } = string.Empty; 
+    public string Country { get; set; } = string.Empty;
+    public int? Duration { get; set; } 
 }
